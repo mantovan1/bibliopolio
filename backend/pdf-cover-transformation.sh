82bce05b37bf92c60391ad1bf15fb2a537ebbd4d
mantovani <g_mantovani@tutanota.com>
2023-08-29 12:47:58 -0300

    refactor: useless folders removed

--- backend/pdf-cover-transformation.sh
+++
@@ -1,9 +0,0 @@
-inotifywait -m ./uploads -e create -e moved_to |
-    while read dir action file; do
-        echo "The file '$file' appeared in directory '$dir' via '$action'"
-
-	name=$(echo ${file/\.pdf/})
-
-	pdftoppm -png -singlefile $dir/$file ./thumbs/$name
-	# do something with the file
-    done
